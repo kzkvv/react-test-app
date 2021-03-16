01718c241294c12333886f0e25700319f4bee40f
Kirill Kazakov <flyinginsilence@gmail.com>
2021-03-16 11:08:12 +0300

    apply patch

--- src/App.js
+++ src/App.js
@@ -1,145 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import {APPLICATION_JSON, BufferEncoders, JsonSerializer, JsonSerializers, RSocketClient,} from 'rsocket-core';
-import RSocketTcpClient from 'rsocket-tcp-client';
-import {RSocketResumableTransport, Utf8Encoders} from "rsocket-core/build";
+import {
+    APPLICATION_JSON,
+    BufferEncoders,
+    encodeCompositeMetadata,
+    MESSAGE_RSOCKET_AUTHENTICATION,
+    MESSAGE_RSOCKET_COMPOSITE_METADATA,
+    RSocketClient,
+} from 'rsocket-core';
 import RSocketWebSocketClient from "rsocket-websocket-client/build";
-import {JsonMetadataSerializer, Metadata} from "./metadata";
-// Create an instance of a client
-
-
-
 
 function App() {
-    let socket;
+    const JSON_METADATA = 'application/vnd.spring.rsocket.metadata+json';
+    const localeAndUserIdJson = JSON.stringify({
+        locale: 'en-EN',
+        userId: 1
+    });
+
+    const authToken = 'Bearer eyJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJmaXQiLCJpYXQiOjE2MTU4ODA0MzQsImV4cCI6MzM0Mzg4MDQzNCwiYWNjb3VudHMiOnsiMSI6eyJzZXNzaW9uSWQiOiI4WjlnREEweFQ5QW16VnA5Vk0wTW5NbFBMbmZ5Nlp6R1pGdUZrU1hPK0pVT1Z3ZEVrWVRCSld0b3JLWlhocUQvUE53bkwwdjlNa1VQcEhJUzB6YUdJaWp1ZjhDQ2M2VEl3N2RqcEhZWnpyST0iLCJ1c2VyUm9sZSI6eyJyb2xlTmFtZSI6IlNlcnZpY2UifSwidXNlck5hbWUiOiJhZG1pbiIsImNvbXBhbnlJZCI6NzEsImNvbXBhbnlOYW1lIjoiUGVua2nFsyBrb250aW5lbnTFsyBiYW5raW7El3MgdGVjaG5vbG9naWpvcywgSlNDIiwiY29tcGFueUNvdW50cnlJZCI6MX19fQ.FTyvvkvCrdSICyOkBhLhwFDfrIM4Z5O1JYqJxt6ByM49U1WgfjAOEswMKqsvqLGWt25TlSxWiqRoCPlp8PDH7bOpTZBazsj2LQ7RzD2A1J0MmmYfW5KNj0GuwZHq-UQTnfRV7b58ivV8I_V7w6UMHbXRC4-4nLzvtvFhOTxZVvTSTCy8KQFryAmbce6YCxsZOQ4-1P5DdLctMNMN_Z5_LfeZAgoA_604R3CUoVvXwTqsglGxOLQo9V8xFzCiKWgrygAc-R2XuZz3jxdU9w4urZ9Wb1vw0W95L0DJhuOSlvcsgy_BYee81L5q6j4zvBO9BQpHcTu5rvWYf9DlMhj7xA';
 
     const client = new RSocketClient({
-        // send/receive objects instead of strings/buffers
-        serializers: {
-            data: JsonSerializer,
-            metadata: JsonMetadataSerializer
-        },
         setup: {
-            debug: true,
-            // ms btw sending keepalive to server
             keepAlive: 60000,
-            // ms timeout if no keepalive response
             lifetime: 180000,
-            // format of `data`
-            dataMimeType: "*/*", //"application/json", //APPLICATION_JSON.toString(),//'application/jsonq',
-            // format of `metadata`
-            metadataMimeType: "*/*", //JsonMetadataSerializer.MIME_TYPE, //"application/json", //APPLICATION_JSON.toString(),
-            // 'application/jsonq',
-            route: '/api/v1/device/DEVICE_NAME/GET_ATM_PROPERTY',
-            locale: "en-EN",
-            userId: 1,
-            'message/x.rsocket.authentication.v0': "Bearer токен",
-            metadata: {
-                "route": '/api/v1/device/DEVICE_NAME/GET_ATM_PROPERTY',
-                "locale": "en-EN",
-                "userId": 1,
-                "message/x.rsocket.authentication.v0": "Bearer токен"
-            }
+            dataMimeType: APPLICATION_JSON.string,
+            metadataMimeType: MESSAGE_RSOCKET_COMPOSITE_METADATA.string,
+            payload: {
+                metadata: encodeCompositeMetadata([
+                    [JSON_METADATA, Buffer.from(localeAndUserIdJson)],
+                    [MESSAGE_RSOCKET_AUTHENTICATION, Buffer.from(authToken)]
+                ])
+            },
         },
-         transport: new RSocketWebSocketClient({url: 'ws://localhost:7080'}, BufferEncoders),
-        // transport: new RSocketTcpClient(
-        //     {host: '127.0.0.1', port: 8080}, // options to Node.js net.connect()
-        //     BufferEncoders,
-        // ),
+        transport: new RSocketWebSocketClient({url: 'ws://localhost:7080'}, BufferEncoders),
     });
 
-    // Open the connection
-    console.log("st")
-    // console.log(client.getConnectionState())
-    // Open the connection
-    // client.connect()
-    //     .then(sub => {
-    //     return client.fetchUserProfile();
-    // })
-    let metadata11 = new Metadata();
-    metadata11.set(Metadata.ROUTE, `/api/v1/device/DEVICE_NAME/GET_ATM_PROPERTY`);
-    metadata11.set(Metadata.AUTHENTICATION_BEARER, "Bearer aaa");
-    metadata11.set("userId", 1);
-    metadata11.set("locale", "en-EN");
+    client.connect().then(
+        (cl) => console.log("Success"),
+        error => console.log(error)
+    )
 
-
-        // client.connect().subscribe({
-        //     onComplete: s => {
-        //         this.socket = s;
-        //         resolve(this.socket);
-        //     },
-        //     onError: error => reject(error),
-        //     onSubscribe: cancel => { this.cancel = cancel}
-        // });
-        //
-        // .then(sub => {
-        //     return new Promise((resolve, reject) => {
-        //         let metadata = new Metadata();
-        //         metadata.set(Metadata.ROUTE, `fetch.profile.${login}`);
-        //         metadata.set(Metadata.AUTHENTICATION_BEARER, this.securityService.getToken());
-        //         socket.requestResponse({
-        //             metadata: metadata11,
-        //         }).subscribe({
-        //             onComplete: msg => resolve(msg.data),
-        //             onError: error => reject(error)
-        //         });
-        //     });
-        // })
-
-    client.connect().subscribe({
-        onComplete: socket => {
-            //this.socket = socket;
-            console.log("1122")
-            // socket provides the rsocket interactions fire/forget, request/response,
-            // request/stream, etc as well as methods to close the socket.
-            let metadata = {
-                route: '/api/v1/device/DEVICE_NAME/GET_ATM_PROPERTY',
-                locale: "en-EN",
-                userId: 1,
-                "message/x.rsocket.authentication.v0": "Bearer токен"
-            };
-            socket.fireAndForget({
-                data: metadata,
-                metadata: metadata11,
-            }).subscribe({
-                onSubscribe: sub => sub.request({
-                    metadata: metadata
-                }),
-                onError: error => console.log(error),
-                onNext: msg => console.log(msg.data),
-                onComplete: () => console.log("qw0")
-            });
-        },
-        onError: error => console.error(error),
-        onSubscribe: cancel => { console.log(cancel)}
-    });
-    // this.socket.fireAndForget(
-    //     {
-    //         data: {some: {json: {value: 1}}},
-    //         metadata: {
-    //             route: '/api/v1/device/DEVICE_NAME/GET_ATM_PROPERTY',
-    //             locale: "en-EN",
-    //             "userId": 1,
-    //             "message/x.rsocket.authentication.v0": "Bearer токен"
-    //         },
-    //     }
-    // )
-
-
-    // client.connect().subscribe({
-    //     onComplete: socket => {
-    //         // socket provides the rsocket interactions fire/forget, request/response,
-    //         // request/stream, etc as well as methods to close the socket.
-    //         console.log("111")
-    //         socket.fireAndForget({
-    //             data: new Buffer("qwe"),
-    //             metadata: new Buffer("qwe1"), // or new Buffer(...)
-    //         });
-    //     },
-    //     onError: error => console.error(error),
-    //     onSubscribe: cancel => {/* call cancel() to abort */
-    //     }
-    // });
     return (
         <div className="App">
             <header className="App-header">
